26a803d74be660e8fa33feb4c022b465a0a38bdc
Vashon Gonzales <104737326+VashonG@users.noreply.github.com>
2023-07-19 22:50:24 -0700

    Imported from Quest

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Dashboard } from "./screens/Dashboard";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import questTheme from 'src/CaratDesignSystemLightTheme';
 
-const router = createBrowserRouter([
-  {
-    path: "/*",
-    element: <Dashboard />,
-  },
-]);
+function App() {
+  return (
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={questTheme}>
+          <Routes></Routes>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </HelmetProvider>
+  );
+}
 
-export const App = () => {
-  return <RouterProvider router={router} />;
-};
+export default App;
